c9fa3f8c9573b7407d17e4954311fa3a00586be4
“deepu” <manideepakgampa21@gmail.com>
2024-12-09 13:22:25 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("HELLOWWWW");
+});
+
+app.get("/animal", (req, res) => {
+  res.send("SANDEEP REDDY MASS");
+});
+
+app.listen(8000, () => {
+  console.log("helloooow");
+});
+g;
